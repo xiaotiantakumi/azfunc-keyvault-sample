e6bb69feecfb5d030d7400879f5d18cd06389b9c
TakumiOda <xiaotiantakumi@gmail.com>
2022-11-03 21:20:18 +0900

    シークレット確認用エンドポイント作成

---
+++ KeyVaultSample1/index.ts
@@ -0,0 +1,14 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const secret = process.env.KEY_VAULT_SECRET;
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: secret,
+  };
+};
+
+export default httpTrigger;
